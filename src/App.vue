deacd87c918c7933c0fb539c66e89c91cada5f87
LXW-PC\LXW <932049492@qq.com>
2019-08-29 22:39:16 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+        </div>
+    </header>
+    <div class="container">
+      <Add :addComment="addComment"/>
+      <List :comments="comments" :deleteComment="deleteComment"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Add from './components/Add.vue'
+import List from './components/List.vue'
+export default{
+  data () {
+    return {
+      comments: [
+        {
+          name: 'Bob',
+          content: 'Vue还不错'
+        },
+        {
+          name: 'Amy',
+          content: 'Vue is good'
+        },
+        {
+          name: 'Cat',
+          content: 'Vue so easy'
+        }
+      ]
+    }
+  },
+  methods: {
+    // 添加评论
+    addComment (comment) {
+      this.comments.unshift(comment)
+    },
+    // 删除指定下标评论
+    deleteComment (index) {
+      this.comments.splice(index, 1)
+    }
+  },
+  components: {
+    Add,
+    List
+  }
+}
+</script>
+
+<style>
+
+</style>

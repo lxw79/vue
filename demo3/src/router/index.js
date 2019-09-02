363c4f09c0287ff0cfedcdc526a2003c92a1ffde
LXW-PC\LXW <932049492@qq.com>
2019-09-02 17:59:09 +0800

    no message

---
+++ demo3/src/router/index.js
@@ -0,0 +1,28 @@
+/**
+ * 路由器
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import About from '../views/About.vue'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  // n个路由
+  routes: [
+    {
+      path: '/about',
+      component: About
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/',
+      redirect: '/about'
+    }
+  ]
+})

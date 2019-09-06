d2891eabb483f9f28e987bda1355d77d7b169416
LXW-PC\LXW <932049492@qq.com>
2019-09-06 22:56:34 +0800

    no message

---
+++ demo6/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>click{{$store.state.count}}times, count is {{evenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
+  </div>
+</template>
+
+<script>
+export default{
+  computed: {
+    evenOrOdd () {
+      return this.$store.getters.evenOrOdd
+    }
+  },
+  methods: {
+    // 增加
+    increment () {
+      // 通知vuex去增加
+      this.$store.dispatch('increment') // 触发store对应的action调用
+    },
+    // 减少
+    decrement () {
+      this.$store.dispatch('decrement')
+    },
+    // 如果是奇数才增加
+    incrementIfOdd () {
+      this.$store.dispatch('incrementIfOdd')
+    },
+    // 过1s才增加
+    incrementAsync () {
+      this.$store.dispatch('incrementAsync')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

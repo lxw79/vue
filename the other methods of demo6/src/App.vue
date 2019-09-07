5c573dfb0160b3d991ff4b8ce925d477ade66d40
LXW-PC\LXW <932049492@qq.com>
2019-09-07 17:19:36 +0800

    no message

---
+++ the other methods of demo6/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>click{{count}}times, count is {{evenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
+  </div>
+</template>
+
+<script>
+import {mapState, mapGetters, mapActions} from 'vuex'
+export default{
+  computed: {
+    ...mapState([`count`]), // mapState()返回值： count () { return this.$store.state[`count`]
+    ...mapGetters([`evenOrOdd`]) // mapGetters()返回值：｛evenOrOdd（）｛return this.$store.getters[`even`]｝｝
+  },
+  methods: {
+    ...mapActions([`increment`, `decrement`, `incrementIfOdd`, `incrementAsync`])
+
+  }
+}
+</script>
+
+<style>
+
+</style>
